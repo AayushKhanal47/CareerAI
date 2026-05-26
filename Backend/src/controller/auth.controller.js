feaae9a01c66fd1c21577182d74582d1f0e5d160
Aayush Khanal <aayushkhanal810@gmail.com>
2026-05-26 19:49:53 +0545

    feat: added auth route and controller

---
+++ Backend/src/controller/auth.controller.js
@@ -0,0 +1,21 @@
+const userModel = require("../models/user.model")
+
+async function registerUserController(req,res) {
+  const  {username, email, password } = req.body
+
+  if(!username||!email||!password) {
+    return res.status(400).json({
+        message:"Plese provide username,email and password"
+    })
+  }
+  const isUserAlreadyExists = await userModel.findOne({
+    $or: [{username}, {email}]
+  })
+  if(isUserAlreadyExists)   {
+    return res.json({message: "Account already exist with username and email"}
+        
+    )
+  }
+}
+
+module.exports = {registerUserController}
