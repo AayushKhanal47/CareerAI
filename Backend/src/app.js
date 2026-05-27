bad640fed0b9d1a989e8a77594d5a4c50a3365ad
Aayush Khanal <aayushkhanal810@gmail.com>
2026-05-27 15:57:21 +0545

    feat:added middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-const authRouter = require("./routes/auth.routes") // IMPORTANT PATH
+const authRouter = require("./routes/auth.routes")
 app.use("/api/auth", authRouter)
 
 module.exports = app
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,10 +1,22 @@
 const { Router } = require("express")
 const authController = require("../controller/auth.controller")
+const authMiddleware = require("../middleware/auth.middleware")
 
 const router = Router()
 
 router.post("/register", authController.registerUserController)
-router.post("/login", authController.loginUserController )
+router.post("/login", authController.loginUserController)
 router.get("/logout", authController.logoutUserController)
+
+// FIXED ROUTE 👇
+router.get(
+    "/get-me",
+    authMiddleware.authUser,
+    (req, res) => {
+        res.json({
+            user: req.user
+        })
+    }
+)
 
 module.exports = router
