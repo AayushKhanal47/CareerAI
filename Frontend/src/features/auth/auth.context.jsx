79e58e38d035fb33edc74f26ac1cf736d9630e98
Aayush Khanal <aayushkhanal810@gmail.com>
2026-06-09 00:31:29 +0545

    auth context

---
+++ Frontend/src/features/auth/auth.context.jsx
@@ -0,0 +1,8 @@
+import { Children, createContext, useState } from "react";
+
+export const AuthContext = createContext()
+
+export const AuthProvider = ({Children}) =>{
+    const [user, setUser] = useState(null)
+    const [loading, setLoading ] = useState(false)
+}
